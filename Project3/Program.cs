fcd3c1141fff123023e8cf65e936df984f47bd61
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-07-11 23:19:57 +0300

    Программа принимает число и проверяет на кратность

---
+++ Project3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход число и проверяет его на кратность 7 и 23
+Console.WriteLine("Введите число");
+int Num = Convert.ToInt32(Console.ReadLine());
+string check (int num1)
+{
+    if (num1 % 7==0 || num1 % 23==0)//или && если число должно быть кратно и 7 и 23
+    {
+        return "число кратно";
+    }
+    else
+    {
+        return "число не кратно";
+    }
+}
+Console.WriteLine(check(Num));
