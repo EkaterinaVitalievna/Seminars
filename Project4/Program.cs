6c04e72912f0b4dba9a51d7cdd699f2ad57d1110
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-07-11 23:42:06 +0300

    Является ли одно число квадратом другого

---
+++ Project4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Является ли одно из двух чисел квадратом другого
+Console.WriteLine("Введите два числа");
+int firstnum = Convert.ToInt32(Console.ReadLine());
+int secondnum = Convert.ToInt32(Console.ReadLine());
+string square (int num1,int num2)
+{
+    if (num1*num1==num2)
+    {
+        return "второе число - квадрат первого";
+    }
+    else
+        if (num2*num2==num1)
+      {
+          return "первое число - квадрат второго";
+      }
+      else
+      {
+        return "ни одно число не является квадратом другого";
+      }
+}
+Console.WriteLine(square(firstnum, secondnum));
+
