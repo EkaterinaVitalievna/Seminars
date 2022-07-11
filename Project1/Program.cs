623119fdcbdb3d0f2ac2ffa99f9e48c0c1d6e411
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-07-11 20:51:51 +0300

    Вывод случайного двузначного числа и выбор наибольшего

---
+++ Project1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+int showNumber (){
+    int num = new Random().Next(10,99);
+    int first = num/10;
+    int second = num%10;
+
+    Console.Write(num+" -> ");
+
+    if(first>second){
+        return first;
+    }
+    else{
+        return second;
+    }
+};
+
+Console.WriteLine(showNumber());
